9cf66624994a8fb3a21db2dccf3122a849953ca0
corneliousfazal <corneliousfazal@gmail.com>
2022-06-01 01:10:02 +0500

    update

---
+++ FilfyRich/Microflake.Core/Domain/Whislist.cs
@@ -0,0 +1,14 @@
+﻿namespace Microflake.Core.Domain
+{
+    public class Whislist : BaseEntity
+    {
+        public long Id { get; set; }
+
+        public string UserID { get; set; }
+
+        public long ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+    }
+}
--- FilfyRich/Microflake.TheComputerShop.Web.UI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Microflake.TheComputerShop.Web.UI.MvcApplication" Language="C#" %>
