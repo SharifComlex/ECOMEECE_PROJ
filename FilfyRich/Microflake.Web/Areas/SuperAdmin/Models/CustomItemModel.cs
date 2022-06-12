1f55b13665ca1c7f3a8ba41c0125d8883af1b413
corneliousfazal <corneliousfazal@gmail.com>
2022-06-12 15:43:32 +0500

    update

---
+++ FilfyRich/Microflake.Web/Areas/SuperAdmin/Models/CustomItemModel.cs
@@ -0,0 +1,11 @@
+﻿using Microflake.Core.Domain;
+using System.Collections.Generic;
+
+namespace Microflake.Web.Areas.SuperAdmin.Models
+{
+    public class CustomItemModel
+    {
+        public List<CustomColor> Colors { get; set; }
+        public List<CustomItem> Items { get; set; }
+    }
+}
--- FilfyRich/Microflake.Web/Controllers/CustomProductController.cs
+++ FilfyRich/Microflake.Web/Controllers/CustomProductController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microflake.Core.Persistence;
+using Microflake.Web.Areas.SuperAdmin.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +13,29 @@ namespace Microflake.Web.Controllers
     public class CustomProductController : Controller
     {
         // GET: CustomProduct
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            using (var _context = new ApplicationDbContext()) {
+                var caps = await _context.CustomColors.ToListAsync();
+                var badges = await _context.CustomItems.ToListAsync();
+
+                return View(new CustomItemModel
+                {
+                    Colors = caps,
+                    Items = badges,
+                });
+            }
+        }
+
+        public async Task<ActionResult> ItemDetail(long ProductId, long ItemId)
+        {
+            using (var _context = new ApplicationDbContext())
+            {
+                var variation = await _context.CustomVariations
+                           .FirstOrDefaultAsync(x=> x.CustomColorId == ProductId && x.CustomItemId == ItemId);
+
+                return Json(variation);
+            }
         }
     }
 }
